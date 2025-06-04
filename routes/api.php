56468a36aac715e081d46d0196558e82bb931f16
Jong44 <mat.skak45@gmail.com>
2025-06-04 22:21:57 +0700

    update cors

--- routes/api.php
+++ routes/api.php
@@ -27,7 +27,7 @@ Route::middleware('auth:api')->group(function () {
 
 
     Route::group([
-        'middleware' => ['role:superadmin', 'cors'],
+        'middleware' => ['role:superadmin'],
     ], function () {
         Route::prefix('outlets')->group(function () {
             Route::get('/', [OutletController::class, 'index']);
